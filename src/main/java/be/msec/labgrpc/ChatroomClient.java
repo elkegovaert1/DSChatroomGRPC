eb1ddbbb266529f36e96dc4c9d73d053bda3d75b
elkegovaert <elke.govaert@outlook.com>
2020-11-14 15:27:07 +0100

    update alles

--- src/main/java/be/msec/labgrpc/ChatroomClient.java
+++ src/main/java/be/msec/labgrpc/ChatroomClient.java
@@ -17,4 +17,35 @@ import be.msec.labgrpc.CalculatorGrpc.CalculatorBlockingStub;
 import be.msec.labgrpc.CalculatorGrpc.CalculatorStub;
 
 public class ChatroomClient {
+    private final ManagedChannel channel;
+    private final ChatroomBlockingStub blockingStub;
+    private final ChatroomStub asyncStub;
+
+    public ChatroomClient(String host, int port){
+        this(ManagedChannelBuilder.forAddress(host, port).usePlaintext(true));
+    }
+
+    public ChatroomClient(ManagedChannelBuilder<?> channelBuilder) {
+        channel = channelBuilder.build();
+        blockingStub = CalculatorGrpc.newBlockingStub(channel);
+        asyncStub = CalculatorGrpc.newStub(channel);
+    }
+
+    public void shutdown() throws InterruptedException {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ChatroomClient client = new ChatroomClient("localhost", 50050);
+        try{
+            client.calculateSum(10, 5);
+
+            client.streamingSum(5);
+
+            client.calculatorHistory();
+        } finally {
+            client.shutdown();
+        }
+    }
+
 }
--- src/main/java/be/msec/labgrpc/ChatroomServer.java
+++ src/main/java/be/msec/labgrpc/ChatroomServer.java
@@ -11,4 +11,54 @@ import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
 
 public class ChatroomServer {
+    private final int port;
+    private final Server server;
+
+    public ChatroomServer(int port) throws IOException {
+        this (ServerBuilder.forPort(port), port);
+    }
+
+    public ChatroomServer(ServerBuilder<?> serverBuilder, int port) {
+        this.port = port;
+        server = serverBuilder.addService(new ServerService()).build();
+    }
+
+    public void start() throws IOException {
+        server.start();
+        System.out.println("Server started, listening on " + port);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("*** shutting down gRPC server since JVM is shutting down");
+            ChatroomServer.this.stop();
+            System.out.println("*** server shut down");
+        }));
+    }
+
+    public void stop() {
+        if (server != null)
+            server.shutdown();
+    }
+
+    public void blockUntilShutdown() throws InterruptedException {
+        if (server != null)
+            server.awaitTermination();
+    }
+
+    public static void main(String[] args) throws Exception{
+        ChatroomServer server = new ChatroomServer(50050);
+        server.start();
+        server.blockUntilShutdown();
+
+    }
+
+    private static class ServerService extends ServerGRPC.ServerImplBase {
+        @Override
+        public void writeMessage(String message) {
+
+        }
+
+        @Override
+        public void getOnlineClients(Empty nul, StreamObserver)
+    }
 }
+
+
